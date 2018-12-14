5350fd6b64cc7cbf4e389a4bce66d22bfd50d578
polin.wei <polin.wei@tw-17102735.globeunion.com>
2018-12-14 15:43:21 +0800

    First Commit

---
+++ src/main/java/com/admin/SbAdminApplication.java
@@ -0,0 +1,19 @@
+package com.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+@SpringBootApplication
+public class SbAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbAdminApplication.class, args);
+	}
+}
